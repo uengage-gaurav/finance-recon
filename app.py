5850dce97861be4936232b40adcef86129d0f1ce
shubhamuengage <111453511+shubhamuengage@users.noreply.github.com>
2025-06-25 17:31:26 +0530

    First commit

--- app.py
+++ app.py
@@ -84,4 +84,4 @@ def process_files(master_path, lsp_path):
 if __name__ == '__main__':
     app.run(debug=True)
 
-app.run(host="0.0.0.0", port=5000, debug=True)
+app.run(host="0.0.0.0", port=6969, debug=True)
